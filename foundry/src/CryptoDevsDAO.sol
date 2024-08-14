08b08d13aa42175ba6b003604cae60f51555bb02
abcode <mabsattar@proton.me>
2024-08-14 06:16:03 +0530

    initialized cryptodevsnft and cryptodevsdao

---
+++ foundry/src/CryptoDevsDAO.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CryptoDevsDAO is Ownable {
+    
+}
---
+++ foundry/src/FakeNFTMarketplace.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract FakeNFTMarketplace {
+    mapping(uint256 => address) public token;
+
+    uint256 nftPrice 0 0.1 ether;
+
+    function purchase(uint256 _tokenId) external payable {
+        require(msg.value == nftPrice, "This NFT costs 0.1 ether");
+        token[_tokenId] = msg.sender;
+    }
+
+    function getPrice() external view returns (uint256) {
+        return nftPrice;
+    }
+
+    /// @dev available() checks whether the given tokenId has already been sold or not
+    /// @param _tokenId - the tokenId to check for
+    function available(uint256 _tokenId) external view returns (bool) {
+        // address(0) = 0x0000000000000000000000000000000000000000
+        // This is the default value for addresses in Solidity
+        if (tokens[_tokenId] == address(0)) {
+            return true;
+        }
+        return false;
+    }
+}
