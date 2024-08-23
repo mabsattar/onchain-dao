28497310ac6c5e2c13eaf30fea4b2907de3dbd0b
abcode <mabsattar@proton.me>
2024-08-23 08:02:46 +0530

    initialized frontend

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,10 @@
+export const CryptoDevsNFTAddress =
+    "0x104a837b2d581601c1ee1C10dCc41c2c6d060110";
+export const FakeNFTMarketplaceAddress =
+    "0x7cF427b88588f718A54A0907410e0F0a5be8eD5F";
+export const CryptoDevsDAOAddress =
+    "0x9c75e75c81707f60bed0327A911cDc04c45a05A6";
+
+export const CryptoDevsNFTABI = []; // REPLACE THIS WITH THE NFT CONTRACT ABI
+export const FakeNFTMarketplaceABI = []; // REPLACE THIS WITH THE FAKE MARKETPLACE ABI
+export const CryptoDevsDAOABI = []; // REPLACE THIS WITH THE DAO ABI
